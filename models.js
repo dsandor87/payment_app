64121934c384975e82af99c806d3f004be5e1b3d
Vavom <nshamash123@gmail.com>
2021-01-27 16:30:29 +0000

    added fields to user

--- models.js
+++ models.js
@@ -1,10 +1,17 @@
 const {Model, DataTypes, Sequelize} = require('sequelize')
 const sequelize = new Sequelize("sqlite:./db.sql", {logging: false})
+
 class User extends Model {}
+
 User.init({
     username: DataTypes.STRING,
     password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    balance: DataTypes.FLOAT,
+    friends: DataTypes.ARRAY,
 }, {sequelize: sequelize})
+
 module.exports = {
     User, sequelize
 }
